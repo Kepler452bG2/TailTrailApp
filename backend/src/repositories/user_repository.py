7e5229e101ce4b098d319b4c56faf7afe8832338
Assel Ibadullayeva <assellyzh@gmail.com>
2025-07-03 22:29:50 +0500

    chore: Decouple backend into its own repository

--- backend/src/repositories/user_repository.py
+++
@@ -1,8 +0,0 @@
-from repositories.base_repository import BaseRepository
-from models.user import User
-
-from dao.sqlalchemy_dao import SQLAlchemyDAO
-
-class UserRepository(BaseRepository[User]):
-    model = User
-    dao_class = SQLAlchemyDAO
