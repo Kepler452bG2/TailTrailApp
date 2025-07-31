920ca7cf6bf875fbc42fa4baa9c7ad05ee0a73f0
Assel Ibadullayeva <assellyzh@gmail.com>
2025-07-31 13:20:51 +0500

    Update iOS app to use port 8667 for WebSocket connections

--- TailTrail/Config.swift
+++ TailTrail/Config.swift
@@ -1,6 +1,6 @@
 import Foundation
 
 struct Config {
-    static let backendURL = "http://209.38.237.102:8080"
-    static let websocketURL = "ws://209.38.237.102:8080"
+    static let backendURL = "http://209.38.237.102:8667"
+static let websocketURL = "ws://209.38.237.102:8667"
 } 
